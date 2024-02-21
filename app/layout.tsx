8fb23f823f88030c077e4eca67fa5031834e4843
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2024-02-21 23:08:47 +0300

    update UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,11 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: {
-        template: '%s | Lango Dashboard',
+        template: 'Lango Dashboard',
         default: 'Lango Dashboard',
     },
-    description: 'The official Next.js Learn Dashboard built with App Router.',
-    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+    description: 'Lango database management',
+    metadataBase: new URL('https://lango-dashboard.vercel.app/'),
 };
 
 export default function RootLayout({
